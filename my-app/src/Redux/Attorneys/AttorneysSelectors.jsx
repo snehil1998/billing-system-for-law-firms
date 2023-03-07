89540f3aa8379f384a153e2036f78f36381e90a3
--global <--global>
2023-03-07 22:54:33 +0000

    remove my-app

--- my-app/src/Redux/Attorneys/AttorneysSelectors.jsx
+++
@@ -1,3 +0,0 @@
-export const getAttorneysData = state => state.attorneys.data;
-
-export const getAttorneysIsLoading = state => state.attorneys.isLoading;
--- my-app/src/Redux/Disbursements/DisbursementsSelectors.jsx
+++
@@ -1,9 +0,0 @@
-export const getDisbursementsData = state => state.disbursements.filteredData;
-
-export const getDisbursementsIsLoading = state => state.disbursements.isLoading;
-
-export const getFromSearchDateForDisbursements = state => state.disbursements.fromSearchDate;
-
-export const getToSearchDateForDisbursements = state => state.disbursements.toSearchDate;
-
-export const getFilterCheckboxesForDisbursements = state => state.disbursements.filterCheckboxes;
